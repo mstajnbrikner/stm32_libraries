3656d713cb127e5203ed7f2c791dd5d8d2d63a9c
Matej Stajnbrikner <stambi28@gmail.com>
2020-09-29 18:17:47 +0200

    lcd first optimization

--- BluePill/lcd_i2c.c
+++ BluePill/lcd_i2c.c
@@ -4,73 +4,105 @@
 
 extern struct lcdMiscData lcdMiscelData;
 
-void startI2C()
+void I2C_start()
 {
 	I2C1->CR1 |= (1 << 8);			//start
 	while(!(I2C1->SR1 & 1)) 
 	{
 	}
-	lcdMiscelData.tempReceive = I2C1->SR1;
+	//lcdMiscelData.tempReceive = I2C1->SR1;
 	
 	I2C1->DR = (0x27 << 1);		//slave address 0x27
 	while(!(I2C1->SR1 & 2))
 	{
 	}
-	lcdMiscelData.tempReceive = I2C1->SR1;
+	//lcdMiscelData.tempReceive = I2C1->SR1;
 	lcdMiscelData.tempReceive = I2C1->SR2;
 }
-void stopI2C()
+void I2C_stop()
 {
 	I2C1->CR1 |= (1 << 9); 		//stop
 }
+
+void I2C_sendByte(int data, int us)
+{
+	I2C1->DR = (data | lcdMiscelData.backlight);
+	while(!(I2C1->SR1 & 4))
+	delayUs(us);
+}
+
+void I2C_sendCommand(int mode, char lNibble, char uNibble)
+{
+	int temp1, temp2;
+	if(mode == MODE_WRITECHAR)
+	{
+		temp1 = 5;
+		temp2 = 1;
+	}
+	else if(mode == MODE_COMMAND)
+	{
+		temp1 = 4;
+		temp2 = 0;
+	}
+	else
+	{}
+	
+	I2C1->DR = 0x0d;
+	while(!(I2C1->SR1 & 4))
+	delayUs(1);
+	
+	I2C1->DR = (uNibble << 4) | temp1 | lcdMiscelData.backlight;
+	while(!(I2C1->SR1 & 4))
+	delayUs(1);
+	
+	I2C1->DR = (uNibble << 4) | temp2 | lcdMiscelData.backlight;
+	while(!(I2C1->SR1 & 4))						
+	delayUs(1);									
+	
+	I2C1->DR = 0x0d;
+	while(!(I2C1->SR1 & 4))
+	delayUs(1);
+	
+	I2C1->DR = (lNibble << 4) | temp1 | lcdMiscelData.backlight;
+	while(!(I2C1->SR1 & 4))
+	delayUs(1);
+	
+	I2C1->DR = (lNibble << 4) | temp2 | lcdMiscelData.backlight;
+	while(!(I2C1->SR1 & 4))
+	delayUs(50);									//wait at least 50 us
+	
+}
+
 void backlightOn()
 {
-	startI2C();
+	I2C_start();
 	
-	I2C1->DR = 0x08;
+	I2C1->DR = BACKLIGHT_ON;
 	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
 	
-	stopI2C();
+	I2C_stop();
 }
+
+void backlightOff()
+{
+	I2C_start();
+	
+	I2C1->DR = BACKLIGHT_OFF;
+	while(!(I2C1->SR1 & 4))
+	
+	I2C_stop();
+}
+
 void writeChar(char ch)
 {
-	startI2C();
-	
 	char upperNibble = (ch & 0xf0) >> 4;
 	char lowerNibble = (ch & 0x0f);
 	
-	I2C1->DR = 0x05;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_start();
 	
-	I2C1->DR = (upperNibble << 4) | 5;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendCommand(MODE_WRITECHAR, lowerNibble, upperNibble);
 	
-	I2C1->DR = (upperNibble << 4) | 1;
-	while(!(I2C1->SR1 & 4))						
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x05;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = (lowerNibble << 4) | 5;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = (lowerNibble << 4) | 1;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us
-	
-	stopI2C();
+	I2C_stop();
 }
 
 void writeString(char* text)
@@ -86,7 +118,6 @@ void setCursor(int row, int column)
 {
 	int address = 0;
 	
-	
 	if(row == 0)
 	{
 		address = column;
@@ -97,278 +128,90 @@ void setCursor(int row, int column)
 	}		
 	else {return;}
 		
-	int lowerNibble = (address & 0x0f);
-	int upperNibble = (address & 0xf0) >> 4;
+	char lowerNibble = (address & 0x0f);
+	char upperNibble = (address & 0xf0) >> 4;
 	upperNibble |= 0x8;
 	
-	startI2C();
+	I2C_start();
 	
-	I2C1->DR = 0x05;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendCommand(MODE_COMMAND, lowerNibble, upperNibble);
 	
-	I2C1->DR = (upperNibble << 4) | 4;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = (upperNibble << 4) | 0;
-	while(!(I2C1->SR1 & 4))						
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x05;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = (lowerNibble << 4) | 4;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = (lowerNibble << 4) | 0;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us
-	
-	stopI2C();
+	I2C_stop();
 }
 
 void lcdInit()
 {
-	RCC->APB2ENR |= 8;					//gpio b clock for pb6 and pb7
-	RCC->APB2ENR |= 1;					//afio clock
-	RCC->APB1ENR |= (1 << 21);			//i2c1 clock enable	
+	//Basic GPIO and I2C setup
+	RCC->APB2ENR |= 8;					
+	RCC->APB2ENR |= 1;					
+	RCC->APB1ENR |= (1 << 21);			
 	
-	GPIOB->CRL &= ~(0xff000000); 		//clear pb6 and pb7
-	GPIOB->CRL |= 0xdd000000;			//pb6 and pb7 - af open drain for i2c
+	GPIOB->CRL &= ~(0xff000000); 		
+	GPIOB->CRL |= 0xdd000000;			
 	
-	I2C1->CR2 |= 0x44;					//peripheral input clock from apb - 36 MHz
-	I2C1->CCR |= 0xb4;					//clock control registers - 100 KHz
-	I2C1->TRISE |= 0x25;				//rising time
-	I2C1->CR1 |= 1;							//peripheral enable
+	I2C1->CR2 |= 0x44;					
+	I2C1->CCR |= 0xb4;					
+	I2C1->TRISE |= 0x25;				
+	I2C1->CR1 |= 1;		
+	//End GPIO and I2C setup
 	
 	delayMs(50);
 	
-	startI2C();
+	lcdMiscelData.backlight = BACKLIGHT_ON;
 	
-	I2C1->DR = 0x00;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(10);
+	I2C_start();
 	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x00, 1);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x34;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x34, 1);
+	I2C_sendByte(0x30, 10000);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x34, 1);
+	I2C_sendByte(0x30, 200);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x30;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayMs(10);									//wait at least 4.1 ms
+	I2C_sendByte(0x34, 1);
+	I2C_sendByte(0x30, 10000);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x24, 1);
+	I2C_sendByte(0x20, 60);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x24, 1);
+	I2C_sendByte(0x20, 1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x84, 1);
+	I2C_sendByte(0x80, 50);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x34;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x00, 1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x84, 1);
+	I2C_sendByte(0x80, 50);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x30;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(200);									//wait at least 100 us
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x00, 1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x14, 1);
+	I2C_sendByte(0x10, 4000);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x00, 1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x64, 1);
+	I2C_sendByte(0x60, 50);
+	I2C_sendByte(0x04, 1);
 	
-	I2C1->DR = 0x34;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0x00, 1);
+	I2C_sendByte(0x04, 1);
+	I2C_sendByte(0xe4, 1);
+	I2C_sendByte(0xe0, 50);
 	
-	I2C1->DR = 0x30;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayMs(10);									//wait at least 4.1 ms
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x24;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x20;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(60);									//wait at least 40 us
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x24;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x20;
-	while(!(I2C1->SR1 & 4))					//FUNCTION SET 
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x84;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x80;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x00;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x84;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x80;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x00;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x14;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x10;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayMs(4);									//wait at least 1.64 ms
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x00;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x64;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x60;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0x00;
-	while(!(I2C1->SR1 & 4))						//DISPLAY ON
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);									
-	
-	I2C1->DR = 0x04;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0xE4;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(1);
-	
-	I2C1->DR = 0xE0;
-	while(!(I2C1->SR1 & 4))
-	lcdMiscelData.tempReceive = I2C1->SR1;
-	delayUs(50);									//wait at least 50 us	
-	
-	stopI2C();
+	I2C_stop();
 }
 
--- BluePill/lcd_i2c.h
+++ BluePill/lcd_i2c.h
@@ -2,14 +2,25 @@
 #define LCD_I2C_H
 #endif
 
+#define BACKLIGHT_ON		0x08
+#define BACKLIGHT_OFF		0x00
+
+#define MODE_WRITECHAR  0x454545
+#define MODE_COMMAND		0x545454
+
 struct lcdMiscData
 {
 	int tempReceive;
+	int backlight;
 };
 
-void startI2C();
-void stopI2C();
+void I2C_start();
+void I2C_stop();
+void I2C_sendByte(int data, int us);
+void I2C_sendCommand(int mode, char lNibble, char uNibble);
+
 void backlightOn();
+void backlightOff();
 void writeChar(char ch);
 void writeString(char* text);
 void setCursor(int row, int column);
